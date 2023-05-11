4447c348232858fd0a4031adb7a199fc3b4678ae
wenceslauthiagon <wenceslauncthiago@outlook.com>
2023-05-11 02:03:50 -0300

    Adjusting and removing tests.

--- innoveahub-api/src/app/news/news.service.spec.ts
+++ innoveahub-api/src/app/news/news.service.spec.ts
@@ -1,18 +1,28 @@
+import { HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
 import { NewsService } from './news.service';
 
 describe('NewsService', () => {
   let service: NewsService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NewsService],
+      providers: [
+        NewsService,
+        {
+          provide: HttpService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<NewsService>(NewsService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(httpService).toBeDefined();
   });
 });
